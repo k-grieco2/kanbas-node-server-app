92599d69d1a91985c073fbc3d07e44c4672930c5
Kiersten Grieco <kierstengrieco@kings.edu>
2024-04-06 00:44:47 -0400

    Finished.

--- Kanbas/assignments/routes.js
+++ Kanbas/assignments/routes.js
@@ -1,18 +1,18 @@
 import db from "../Database/index.js";
 function AssignmentRoutes(app) {
-    app.put("/api/assignments/:mid", (req, res) => {
-        const { mid } = req.params;
+    app.put("/api/assignments/:aid", (req, res) => {
+        const { aid } = req.params;
         const assignmentIndex = db.assignments.findIndex(
-          (a) => a._id === mid);
+          (a) => a._id === aid);
         db.assignments[assignmentIndex] = {
           ...db.assignments[assignmentIndex],
           ...req.body
         };
         res.sendStatus(204);
     });    
-    app.delete("/api/assignments/:mid", (req, res) => {
-        const { mid } = req.params;
-        db.assignments = db.assignments.filter((a) => a._id !== mid);
+    app.delete("/api/assignments/:aid", (req, res) => {
+        const { aid } = req.params;
+        db.assignments = db.assignments.filter((a) => a._id !== aid);
         res.sendStatus(200);
     });
     app.post("/api/courses/:cid/assignments", (req, res) => {
@@ -24,11 +24,10 @@ function AssignmentRoutes(app) {
         };
         db.assignments.push(newAssignment);
         res.send(newAssignment);
-      });    
+    });    
     app.get("/api/courses/:cid/assignments", (req, res) => {
         const { cid } = req.params;
-        const assignments = db.assignments
-        .filter((a) => a.course === cid);
+        const assignments = db.assignments.filter((a) => a.course === cid);
         res.send(assignments);
     });
 }
