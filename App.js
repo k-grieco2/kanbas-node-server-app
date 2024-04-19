0e90888e2eb83b3797ddd6e5bd1973f0da95f513
Kiersten Grieco <kierstengrieco@kings.edu>
2024-04-19 02:35:37 -0400

    Modifications made for render.com

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import AssignmentRoutes from './Kanbas/assignments/routes.js';
 import UserRoutes from './Users/routes.js';
 import session from "express-session";
 import "dotenv/config";
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
 mongoose.connect(CONNECTION_STRING);
 const app = express();
 const sessionOptions = {
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV !== "development") {
     sessionOptions.cookie = {
       sameSite: "none",
       secure: true,
-      domain: process.env.HTTP_SERVER_DOMAIN,
+      // domain: process.env.HTTP_SERVER_DOMAIN, commented out for render.com purposes
     };
 }  
 app.use(session(sessionOptions));
