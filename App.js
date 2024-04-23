bd6a010993bb40cfc108c895020977d3da0235ad
Kiersten Grieco <kierstengrieco@kings.edu>
2024-04-23 11:08:23 -0400

    Update App.js to not include local db.

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import AssignmentRoutes from './Kanbas/assignments/routes.js';
 import UserRoutes from './Users/routes.js';
 import session from "express-session";
 import "dotenv/config";
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
 mongoose.connect(CONNECTION_STRING);
 const app = express();
 const sessionOptions = {
